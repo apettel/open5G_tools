c3e329ddcc282df86236af576aa39ba1d684a53b
Benjamin Menkuec <benjamin@menkuec.de>
2023-04-22 20:09:54 +0200

    first working revision of server only read mem has beeen tested so far with echo -n -e "\x0d\x00\x00\x00\x00\x0c\x40\x44\x7c\x0c\x00\x44\x7c\x0c\x80\x44\x7c" | nc 192.168.137.2 69

--- server/server.c
+++ server/server.c
@@ -8,27 +8,103 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <signal.h>
-
-#define MAX_BUF_SIZE 1024
+#include <stdint.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 
 int running = 1;
+int fd;
+void *map_base;
+const off_t map_start_addr = 0x7C440000; // needs to be page aligned
+const unsigned map_width = 0x10000; // needs to be page aligned
+// fifo core is at 0x 7C 44 00 00
+// pss core is at  0x 7C 44 40 00
+// rx core is at   0x 7C 44 80 00
+int sockfd, newsockfd, portno;
+unsigned page_size, mapped_size;
 
 void handle_sigint(int sig)
 {
     running = 0;
 }
 
+void handle_rx_msg(char *msg, uint32_t len)
+{
+    void *virt_addr;
+    char *reply_msg = (char*) malloc(len);
+    switch(msg[0])
+    {
+        case 0:
+            // read memory
+            reply_msg[0] = 0;
+            for (int i = 0; i < (len - 1)/4; i++)
+            {
+                unsigned read_addr = *(uint32_t*)(msg + 1 + i*4);
+                virt_addr = map_base + read_addr - (unsigned)map_start_addr;
+                uint32_t mem_data = *(volatile uint32_t*)virt_addr;
+                *(uint32_t*)(reply_msg + 1 + i*4) = mem_data;
+                printf("reading from addr %08x -> %08x\n", read_addr, mem_data);
+            }
+            // send reply
+            printf("sending reply: ");
+            for (int i = 0; i < len; i++)  printf("%02x ", reply_msg[i]);
+            printf("\n");
+            if (send(newsockfd, &len, sizeof(len), 0) == -1) {
+                perror("send");
+                exit(EXIT_FAILURE);
+            }
+
+            if (send(newsockfd, reply_msg, len, 0) == -1) {
+                perror("send");
+                exit(EXIT_FAILURE);
+            }
+        break;
+        case 1:
+            // write memory
+            for (int i = 0; i < (len - 1)/8; i++)
+            {
+                unsigned write_addr = *(uint32_t*)(msg + 1 + i*8);
+                virt_addr = map_base + write_addr - (unsigned)map_start_addr;
+                uint32_t mem_data = *(uint32_t*)(msg + 1 + 4 + i*8);
+                printf("write %08x to addr %08x\n", mem_data, mem_data);
+                *(volatile uint32_t*)virt_addr = mem_data;
+            }
+        break;
+        default:
+            printf("ERROR: unknown message type!");
+        break;
+    }
+    free(reply_msg);    
+}
+
+void map_memory()
+{
+	fd = open("/dev/mem", O_RDWR | O_SYNC);
+	mapped_size = page_size = getpagesize();
+    // unsigned offset_in_page = (unsigned)map_start_addr & (page_size - 1);
+    // mapped_size = ceil(map_width/page_size)
+    mapped_size = map_width;
+	map_base = mmap(NULL,
+			mapped_size,
+			PROT_READ | PROT_WRITE,
+			MAP_SHARED,
+			fd,
+			map_start_addr);
+	if (map_base == MAP_FAILED)
+        perror("ERROR mmap");
+    printf("page size = %08x, map_base = %p\n", page_size, map_base);
+}
+
+void unmap_memory(){
+    if (munmap(map_base, mapped_size) == -1)
+        perror("ERROR munmap");
+    close(fd);    
+}
+
 int main(int argc, char *argv[]) {
-    int sockfd, newsockfd, portno;
     unsigned int clilen;
     struct sockaddr_in serv_addr, cli_addr;
-    //char buffer[MAX_BUF_SIZE];
     int n;
-
-    if (argc < 2) {
-        fprintf(stderr,"ERROR, no port provided\n");
-        exit(1);
-    }
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
@@ -48,55 +124,77 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    listen(sockfd,5);
-    clilen = sizeof(cli_addr);
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-    if (newsockfd < 0) {
-        perror("ERROR on accept");
-        exit(1);
-    }
-
-    // Send variable-length data
-    char* data = "hello, world!";
-    int data_len = strlen(data) + 1; // Add 1 for the null terminator
-
-    if (send(newsockfd, &data_len, sizeof(data_len), 0) == -1) {
-        perror("send");
-        exit(EXIT_FAILURE);
-    }
-
-    if (send(newsockfd, data, data_len, 0) == -1) {
-        perror("send");
-        exit(EXIT_FAILURE);
-    }
+    printf("mapping memory...\n");
+    map_memory();
+    printf("memory mapped\n");
 
     // Set up SIGINT signal handler
-    signal(SIGINT, handle_sigint);
-    
-    while (running) {
+    struct sigaction a;
+    a.sa_handler = handle_sigint;
+    a.sa_flags = 0;
+    sigemptyset( &a.sa_mask );
+    sigaction( SIGINT, &a, NULL );
 
-        // Receive variable-length data
-        int recv_data_len;
+    listen(sockfd,5);
+    printf("server running on port %d\n", portno);
 
-        if ((n = recv(newsockfd, &recv_data_len, sizeof(recv_data_len), 0)) == -1) {
-            perror("recv");
-            exit(EXIT_FAILURE);
+    unsigned alloc_size = 1024;
+    char* recv_data = (char*) malloc(alloc_size);
+    while(running)
+    {
+
+        clilen = sizeof(cli_addr);
+        printf("waiting for client...\n");
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+        if (newsockfd < 0) {
+            perror("ERROR on accept");
+            exit(1);
         }
+        printf("client connected\n");
 
-        char* recv_data = (char*) malloc(recv_data_len);
+        uint32_t recv_data_len;
+        while (running) {
+            if ((n = recv(newsockfd, &recv_data_len, sizeof(recv_data_len), MSG_WAITALL)) == -1) {
+                perror("recv");
+                exit(EXIT_FAILURE);
+            }
 
-        if ((n = recv(newsockfd, recv_data, recv_data_len, 0)) == -1) {
-            perror("recv");
-            exit(EXIT_FAILURE);
+            if (recv_data_len > alloc_size)
+            {
+                // realloc buffer if recv size is larger than allocated memory
+                free(recv_data);
+                alloc_size = recv_data_len;
+                char* recv_data = (char*) malloc(alloc_size);
+            }
+
+            if ((n = recv(newsockfd, recv_data, recv_data_len, MSG_WAITALL)) == -1) {
+                perror("recv");
+                exit(EXIT_FAILURE);
+            }
+
+            if (n == 0)
+            {
+                printf("Client disconnected\n");
+                break;
+            }
+
+            if (recv_data_len != 0)
+            {
+                printf("Received %d bytes: ", recv_data_len);
+                for (unsigned int i = 0; i < recv_data_len; i++)
+                {
+                    printf("%02x ", recv_data[i]);
+                }
+                printf("\n");
+                handle_rx_msg(recv_data, recv_data_len);
+            }
         }
-
-        printf("Received data: %s\n", recv_data);
-        free(recv_data);
     }
 
-    // Cleanup
     close(newsockfd);
     close(sockfd);
+    // unmap_memory();
+    free(recv_data);
 
     return 0;
 }
